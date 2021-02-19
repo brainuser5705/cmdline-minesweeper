3cece9927f3059cea74422b216a292a5eaf7ae6d
brainuser5705 <76625487+brainuser5705@users.noreply.github.com>
2021-02-19 10:16:10 -0500

    Push (#7)
    * implemented gameover mode and level enum
    
    * deleted files
    
    Co-authored-by: Ashley <acl9213@rit.edu>

--- src/MinesweeperGame.java
+++ src/MinesweeperGame.java
@@ -3,29 +3,33 @@ import java.util.Scanner;
 
 public class MinesweeperGame {
 
-    Field game;
-    int numMines;
-    boolean isGameOver;
-    boolean isWinner;
-    int flagsLeft;
+    private final Field field;
+    private int flagsLeft;
+    private int gameOverMode = 1;
+
+    private boolean isGameOver;
+    private boolean isWinner;
 
     public MinesweeperGame(int numMines, int numRows, int numCols){
-        game = new Field(numMines, numRows, numCols);
-        game.generateField();
-        this.numMines = numMines;
+        field = new Field(numMines, numRows, numCols);
         isGameOver = false;
         isWinner = true;
         flagsLeft = numMines;
+    }
+
+    public MinesweeperGame(Level level){
+        field = level.getGame();
+        flagsLeft = level.getNumMines();
     }
 
     public void playMinesweeper(){
         Scanner s = new Scanner(System.in);
 
         while(!isGameOver){
-            game.printField();
+            field.printField();
             String c = s.nextLine();
             //"(r|f|uf) ((\\d)+,(\\d)+( )?)+"
-            if (c.matches(generateRegex()) || c.equals("p") || c.equals("q") || c.equals("auto")){
+            if (c.matches(generateRegex())){ // make a command class?
                 String[] args = c.split(" ");
                 switch(args[0]){
                     case "r" -> {
@@ -38,20 +42,37 @@ public class MinesweeperGame {
                         unflagPositions(args);
                     }
                     case "p" -> {
-                        game.printField();
+                        field.printField();
                     }
                     case "q" -> {
-                        game.revealField();
+                        field.revealField();
                         System.out.println("Don't give up next time!");
                         isGameOver = true;
                     }
                     case "auto" -> {
                         autoPlay();
                     }
+                    case "reset" -> {
+                        resetGame();
+                    }
+                    case "gameover" -> {
+                        gameOverMode = switch(args[1]){ // no need for try because regex filters out invalid commands
+                            case "1" -> 1;
+                            case "2" -> 2;
+                            case "3" -> 3;
+                            default -> throw new IllegalStateException("Unexpected value: " + args[1]);
+                        };
+                        System.out.println("Gameover change to: " + args[1]);
+                    }
                     default -> System.out.println("This command is not supported yet.");
                 }
                 if (!isGameOver) {
-                    isGameOver = realGameOver();
+                    isGameOver = switch(gameOverMode){
+                        case 1 -> gameOver1();
+                        case 2 -> gameOver2();
+                        case 3 -> gameOver3();
+                        default -> throw new IllegalStateException("Unexpected value: " + gameOverMode);
+                    };
                 }
                 System.out.println("Flags left: " + flagsLeft);
             }else {
@@ -59,7 +80,7 @@ public class MinesweeperGame {
             }
         }
 
-        game.revealField();
+        field.revealField();
         if (isWinner)
             System.out.println("You win!");
         else
@@ -67,15 +88,15 @@ public class MinesweeperGame {
     }
 
     private String generateRegex(){
-        String rowLimit = "[0-" + (game.getNumRows() - 1) + "]";
-        String colLimit = "[0-" + (game.getNumCols() - 1) + "]";
-        return "(r|f|uf) ((" + rowLimit + ")+,(" + colLimit + ")+( )?)+";
+        String rowLimit = "[0-" + (field.getNumRows() - 1) + "]";
+        String colLimit = "[0-" + (field.getNumCols() - 1) + "]";
+        return "((r|f|uf) ((" + rowLimit + ")+,(" + colLimit + ")+( )?)+)|p|q|n|reset|auto|(gameover [1-3])";
     }
 
     private void revealPositions(String[] args){
         for (int i = 1; i < args.length; i++){
             String[] coords = args[i].split(",");
-            Block s = game.getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+            Block s = field.getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
             if (s.isMine() && !s.isFlag()){ // not working
                 isWinner = false;
                 isGameOver = true;
@@ -92,7 +113,7 @@ public class MinesweeperGame {
     private void flagPositions(String[] args){
         for (int i = 1; i < args.length; i++){
             String[] coords = args[i].split(",");
-            Block s = game.getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+            Block s = field.getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
             s.flag();
             flagsLeft--;
         }
@@ -101,19 +122,18 @@ public class MinesweeperGame {
     private void unflagPositions(String[] args){
         for (int i = 1; i < args.length; i++){
             String[] coords = args[i].split(",");
-            Block s = game.getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+            Block s = field.getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
             s.unflag();
             flagsLeft++;
         }
     }
 
-    //maybe use an array of already explored squares
     private void revealSurroundingBlanks(Block s, ArrayList<Block> revealedSquares){
         int[][] indexes = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 
         for (int[] pair : indexes) {
             try {
-                Block adjacentBlock = game.getBlock(s.getRow()+pair[0], s.getCol()+pair[1]);
+                Block adjacentBlock = field.getBlock(s.getRow()+pair[0], s.getCol()+pair[1]);
                 if (!revealedSquares.contains(adjacentBlock) && adjacentBlock.isBlankBlock()){ // is already revealed, then skip - add this function
                     adjacentBlock.reveal();
                     revealAdjacentBlock(s); //to show numbers
@@ -131,7 +151,7 @@ public class MinesweeperGame {
 
         for (int[] pair : indexes) {
             try {
-                Block adjacentSquare = game.getBlock(s.getRow()+pair[0], s.getCol()+pair[1]);
+                Block adjacentSquare = field.getBlock(s.getRow()+pair[0], s.getCol()+pair[1]);
                 if (!adjacentSquare.isMine()) {
                     adjacentSquare.reveal();
                 }
@@ -142,7 +162,7 @@ public class MinesweeperGame {
     }
 
     private boolean areAllMinesFlagged(){
-        MineBlock[] mineCoords = game.getMineCoords();
+        MineBlock[] mineCoords = field.getMineCoords();
         for (MineBlock mine : mineCoords){
             if (mine.isFlag()) {
                 return false;
@@ -151,8 +171,8 @@ public class MinesweeperGame {
         return true;
     }
 
-    public boolean realGameOver(){ // game over condition is all blocks are revealed and all mines are flagged - this will eliminate guessing (in a way)
-        Block[][] field = game.getField();
+    private boolean gameOver1(){ // game over condition is all blocks are revealed and all mines are flagged - this will eliminate guessing (in a way)
+        Block[][] field = this.field.getField();
         for (Block[] row : field){
             for (Block block : row){
                 if ((block.isMine() && !block.isFlag()) || (!block.isMine() && !block.isReveal())){
@@ -160,11 +180,33 @@ public class MinesweeperGame {
                 }
             }
         }
+        flagsLeft = 0;
+        isWinner = true;
         return true;
     }
 
-    public void autoPlay(){
-        Block[][] field = game.getField();
+    private boolean gameOver2(){ // if all flags are used and all mines are flagged
+        if (flagsLeft == 0 && areAllMinesFlagged()) {
+            isWinner = true;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean gameOver3(){ // game over condition is all blocks are revealed
+        Block[][] field = this.field.getField();
+        for (Block[] row : field){
+            for (Block block : row){
+                if (!block.isMine() && !block.isReveal()) return false;
+            }
+        }
+        flagsLeft = 0;
+        isWinner = true;
+        return true;
+    }
+
+    private void autoPlay(){
+        Block[][] field = this.field.getField();
         for (Block[] row : field){
             for (Block block : row){
                 if (block.isMine()) {
@@ -177,9 +219,14 @@ public class MinesweeperGame {
         }
     }
 
-    private boolean isGameOver(){
-        return flagsLeft == 0 && areAllMinesFlagged();
-        // if all flags are used and all mines are flagged
+    private void resetGame(){
+        Block[][] field = this.field.getField(); // make this a instance var
+        for (Block[] row : field){
+            for (Block block : row){
+                block.forceUnreveal();
+            }
+        }
+        flagsLeft = this.field.getNumMines();
     }
 
 }
