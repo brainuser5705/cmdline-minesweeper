2d468f95b2b8cbba4556d722ada41096640731d2
Ashley <brainuser5705@protonmail.ch>
2021-03-03 00:01:04 -0500

    Preliminary fix for regex matching - try, catch statements for all action methods with a general (\d) regex statement

--- src/MinesweeperGame.java
+++ src/MinesweeperGame.java
@@ -1,9 +1,13 @@
+import build.Block;
+import build.Field;
+import build.Level;
+import build.MineBlock;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MinesweeperGame {
+public class MinesweeperGame extends Field{
 
-    private final Field field;
     private int flagsLeft;
     private int gameOverMode = 1;
 
@@ -11,14 +15,14 @@ public class MinesweeperGame {
     private boolean isWinner;
 
     public MinesweeperGame(int numMines, int numRows, int numCols){
-        field = new Field(numMines, numRows, numCols);
+        super(numMines, numRows, numCols);
         isGameOver = false;
         isWinner = true;
         flagsLeft = numMines;
     }
 
     public MinesweeperGame(Level level){
-        field = level.getGame();
+        super(level.getNumMines(), level.getNumRows(), level.getNumCols());
         flagsLeft = level.getNumMines();
     }
 
@@ -26,10 +30,11 @@ public class MinesweeperGame {
         Scanner s = new Scanner(System.in);
 
         while(!isGameOver){
-            field.printField();
+            printField();
             String c = s.nextLine();
             //"(r|f|uf) ((\\d)+,(\\d)+( )?)+"
-            if (c.matches(generateRegex())){ // make a command class?
+//            if (c.matches(generateRegex())){ // make a command class?
+            if (c.matches("((r|f|uf) (\\d+,\\d+( )?)+)|p|q|n|reset|auto|(gameover [1-3])")){ // will have index error if digits aren't in bound
                 String[] args = c.split(" ");
                 switch(args[0]){
                     case "r" -> {
@@ -42,10 +47,10 @@ public class MinesweeperGame {
                         unflagPositions(args);
                     }
                     case "p" -> {
-                        field.printField();
+                        printField();
                     }
                     case "q" -> {
-                        field.revealField();
+                        revealField();
                         System.out.println("Don't give up next time!");
                         isGameOver = true;
                     }
@@ -80,7 +85,7 @@ public class MinesweeperGame {
             }
         }
 
-        field.revealField();
+        revealField();
         if (isWinner)
             System.out.println("You win!");
         else
@@ -88,25 +93,31 @@ public class MinesweeperGame {
     }
 
     private String generateRegex(){
-        String rowLimit = "[0-" + (field.getNumRows() - 1) + "]";
-        String colLimit = "[0-" + (field.getNumCols() - 1) + "]";
+        String rowLimit = "[0-" + (getNumRows() - 1) + "]";
+        String colLimit = "[0-" + (getNumCols() - 1) + "]";
         return "((r|f|uf) ((" + rowLimit + ")+,(" + colLimit + ")+( )?)+)|p|q|n|reset|auto|(gameover [1-3])";
     }
 
     private void revealPositions(String[] args){
         for (int i = 1; i < args.length; i++){
             String[] coords = args[i].split(",");
-            Block s = field.getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
-            if (s.isMine() && !s.isFlag()){ // not working
-                isWinner = false;
-                isGameOver = true;
-                break;
-            }else {
-                s.reveal();
-                ArrayList<Block> revealedBlocks = new ArrayList<Block>();
-                revealedBlocks.add(s); // see if i can directly initialize
-                revealSurroundingBlanks(s, revealedBlocks);
+
+            try {
+                Block s = getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+                if (s.isMine() && !s.isFlag()) { // not working
+                    isWinner = false;
+                    isGameOver = true;
+                    break;
+                } else {
+                    s.reveal();
+                    ArrayList<Block> revealedBlocks = new ArrayList<Block>();
+                    revealedBlocks.add(s); // see if i can directly initialize
+                    revealSurroundingBlanks(s, revealedBlocks);
+                }
+            } catch (IndexOutOfBoundsException e){ // for general regex (\d) if coordinates are out of bounds
+                System.out.println("Invalid coordinates.");
             }
+
         }
     }
 
@@ -116,12 +127,18 @@ public class MinesweeperGame {
         }else{
             for (int i = 1; i < args.length; i++) {
                 String[] coords = args[i].split(",");
-                Block s = field.getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
-                s.flag();
-                if (flagsLeft > 0 && !s.isReveal()) // not already revealed
-                    flagsLeft--;
-                else
-                    break;
+
+                try{
+                    Block s = getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+                    s.flag();
+                    if (flagsLeft > 0 && !s.isReveal()) // not already revealed
+                        flagsLeft--;
+                    else
+                        break;
+                }catch (IndexOutOfBoundsException e){
+                    System.out.println("Invalid coordinates.");
+                }
+
             }
         }
     }
@@ -129,9 +146,13 @@ public class MinesweeperGame {
     private void unflagPositions(String[] args){
         for (int i = 1; i < args.length; i++){
             String[] coords = args[i].split(",");
-            Block s = field.getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
-            s.unflag();
-            flagsLeft++;
+            try{
+                Block s = getBlock(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+                s.unflag();
+                flagsLeft++;
+            }catch (IndexOutOfBoundsException e){
+                System.out.println("Invalid coordinates.");
+            }
         }
     }
 
@@ -140,7 +161,7 @@ public class MinesweeperGame {
 
         for (int[] pair : indexes) {
             try {
-                Block adjacentBlock = field.getBlock(s.getRow()+pair[0], s.getCol()+pair[1]);
+                Block adjacentBlock = getBlock(s.getRow()+pair[0], s.getCol()+pair[1]);
                 if (!revealedSquares.contains(adjacentBlock) && adjacentBlock.isBlankBlock()){ // is already revealed, then skip - add this function
                     adjacentBlock.reveal();
                     revealAdjacentBlock(s); //to show numbers
@@ -153,12 +174,13 @@ public class MinesweeperGame {
         }
     }
 
+    // util method for reveal surrounding blocks
     private void revealAdjacentBlock(Block s){
         int[][] indexes = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 
         for (int[] pair : indexes) {
             try {
-                Block adjacentSquare = field.getBlock(s.getRow()+pair[0], s.getCol()+pair[1]);
+                Block adjacentSquare = getBlock(s.getRow()+pair[0], s.getCol()+pair[1]);
                 if (!adjacentSquare.isMine()) {
                     adjacentSquare.reveal();
                 }
@@ -169,7 +191,7 @@ public class MinesweeperGame {
     }
 
     private boolean areAllMinesFlagged(){
-        MineBlock[] mineCoords = field.getMineCoords();
+        MineBlock[] mineCoords = getMineCoords();
         for (MineBlock mine : mineCoords){
             if (!mine.isFlag()) {
                 return false;
@@ -179,7 +201,7 @@ public class MinesweeperGame {
     }
 
     private boolean gameOver1(){ // game over condition is all blocks are revealed and all mines are flagged - this will eliminate guessing (in a way)
-        Block[][] field = this.field.getField();
+        Block[][] field = getField();
         for (Block[] row : field){
             for (Block block : row){
                 if ((block.isMine() && !block.isFlag()) || (!block.isMine() && !block.isReveal())){
@@ -201,7 +223,7 @@ public class MinesweeperGame {
     }
 
     private boolean gameOver3(){ // game over condition is all blocks are revealed
-        Block[][] field = this.field.getField();
+        Block[][] field = getField();
         for (Block[] row : field){
             for (Block block : row){
                 if (!block.isMine() && !block.isReveal()) return false;
@@ -213,7 +235,7 @@ public class MinesweeperGame {
     }
 
     private void autoPlay(){
-        Block[][] field = this.field.getField();
+        Block[][] field = getField();
         for (Block[] row : field){
             for (Block block : row){
                 if (block.isMine()) {
@@ -227,13 +249,13 @@ public class MinesweeperGame {
     }
 
     private void resetGame(){
-        Block[][] field = this.field.getField(); // make this a instance var
+        Block[][] field = getField(); // make this a instance var
         for (Block[] row : field){
             for (Block block : row){
                 block.forceUnreveal();
             }
         }
-        flagsLeft = this.field.getNumMines();
+        flagsLeft = getNumMines();
     }
 
 }
